3778c5d6a7fd58f695def368491495330282cc58
alex <contato@alexbraga.net>
2013-10-07 15:21:52 -0300

    Mudana no payment

--- app/code/local/MagentoNet/Cielo/Model/Payment.php
+++ app/code/local/MagentoNet/Cielo/Model/Payment.php
@@ -18,7 +18,7 @@ class MagentoNet_Cielo_Model_Payment extends Mage_Payment_Model_Method_Cc
 {
 
     protected $_code  = 'cielo';
-    //protected $_formBlockType = 'cielo/form';
+    protected $_formBlockType = 'cielo/form';
     //protected $_infoBlockType = 'cielo/info';
 
     //Is this payment method a gateway (online auth/charge) ?
